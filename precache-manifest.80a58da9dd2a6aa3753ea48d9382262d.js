515047c34eaa76b97eda495f253887422f018038
Arturo Balbin Cibertec <a.balbin.r@gmail.com>
2019-09-16 21:26:18 -0500

    Updates

---
+++ precache-manifest.80a58da9dd2a6aa3753ea48d9382262d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a0dcd0cf1604507702e6db1911bf86",
+    "url": "/react-wd-test/index.html"
+  },
+  {
+    "revision": "d22ed44f00bcdeaf176c",
+    "url": "/react-wd-test/static/css/main.0849df17.chunk.css"
+  },
+  {
+    "revision": "90b827442687801a100a",
+    "url": "/react-wd-test/static/js/2.615d2d0b.chunk.js"
+  },
+  {
+    "revision": "d22ed44f00bcdeaf176c",
+    "url": "/react-wd-test/static/js/main.b8f54b61.chunk.js"
+  },
+  {
+    "revision": "40b1c32a72e480b04140",
+    "url": "/react-wd-test/static/js/runtime~main.9a0effca.js"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/react-wd-test/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/react-wd-test/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/react-wd-test/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/react-wd-test/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/react-wd-test/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/react-wd-test/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/react-wd-test/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/react-wd-test/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/react-wd-test/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/react-wd-test/static/media/fa-solid-900.d6d8d5da.woff2"
+  }
+]);
